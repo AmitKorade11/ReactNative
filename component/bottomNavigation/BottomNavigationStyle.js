be2ff2f79253a62fbdd423862fce464707b5fbaf
AmitKorade11 <amitkorade11@gmail.com>
2024-02-17 17:03:57 +0530

    deleting commented logs

--- component/bottomNavigation/BottomNavigationStyle.js
+++ component/bottomNavigation/BottomNavigationStyle.js
@@ -4,5 +4,4 @@ export const bnStyle = StyleSheet.create({
   bottomBar: { flex: 1, flexDirection: "row" },
   menuButton: { flex: 1, justifyContent: "space-evenly", alignItems: "center" },
   menuTitle: { fontSize: 15, color: "black", fontWeight: "bold" },
-  //   darkgreen shade - #33691E
 });
